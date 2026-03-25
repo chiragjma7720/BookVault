7ef399553a60d375dc473782cd647425cbfd80ac
Chirag Patel <chiragjma7720@gmail.com>
2026-03-25 14:42:53 +0530

    first commit

---
+++ src/test/java/com/solvative/bookvault/BookvaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solvative.bookvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookvaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
